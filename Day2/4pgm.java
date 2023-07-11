c611d323ffd78d3e7f22af7ecf37f2529cf63fa0
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-11 10:14:28 +0530

    Create 4pgm.java

---
+++ Day2/4pgm.java
@@ -0,0 +1,11 @@
+
+public class Hundredto0 {
+    public static void main(String[] args) {
+        for(int i = 100; i>=0; i--){
+           
+            System.out.println(i);
+            
+        }
+        
+    }
+}
