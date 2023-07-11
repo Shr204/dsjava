69f4fc216d8e6c7b87bb8284c67f77cbcdce2e6e
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-11 09:57:08 +0530

    Create 2pgm.java

---
+++ Day1/2pgm.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class gof2 {
+    public static void main(String[] args) {
+        int n1,n2;
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        if (n1>n2){
+            System.out.println("Number 1 is greater");
+        }
+        else {
+            System.out.println("Number 2 is greater");
+        }
+        
+        
+    }
+}
