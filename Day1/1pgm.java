e20a774d8e9da1cf990cf9a4b0cc3ab08baaf11e
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-11 09:47:44 +0530

    Create 1pgm.java

---
+++ Day1/1pgm.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class posnegzero {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        if (n>0){
+            System.out.println("The number is positive");
+        }
+        else if (n<0){
+            System.out.println("The number is negative");
+        }
+        else {
+            System.out.println("The number is zero");
+        }
+    }
+}
