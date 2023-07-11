08aed334a31dde86177e951d891f70eaa5e4756d
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-11 09:59:12 +0530

    Create 3pm.java

---
+++ Day1/3pm.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class evorod {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+       
+        if (n%2==0){
+            System.out.println("Number is EVEN");
+        }
+        else {
+            System.out.println("Number is ODD");
+        }
+        
+        
+    }
+}
