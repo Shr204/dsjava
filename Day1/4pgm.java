333b9a08dc997cb8aab9dbfa42eb4ba9af516588
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-11 10:02:17 +0530

    Create 4pgm.java

---
+++ Day1/4pgm.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class evorod {
+    public static void main(String[] args) {
+        int n1, n2, sum;
+        Scanner sc = new Scanner(System.in);
+        n1 = sc.nextInt();
+        n2 = sc.nextInt();
+        sum = n1+n2;
+        if (sum%2==0){
+            System.out.println("Sum is EVEN");
+        }
+        else {
+            System.out.println("Sum is ODD");
+        }
+        
+        
+    }
+}
