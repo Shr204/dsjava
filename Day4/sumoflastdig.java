333a878e3e2dc6e8e60066624f6e61d0d31dcf4e
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-13 10:07:51 +0530

    Update sumoflastdig.java

--- Day4/sumoflastdig.java
+++ Day4/sumoflastdig.java
@@ -1,1 +1,15 @@
 import java.io.*;
+import java.util.*;
+class UserMainCode
+{
+  public int addLastDigits(int input1, int input2){
+    if(input1<0){
+      input1 = input1*-1;
+    }
+    if(input2<0){
+      input2 = input2*-1;
+    }
+    int n1 = input1%10;
+    int n2 = input2%10;
+    int sum = n1 + n2;
+}
