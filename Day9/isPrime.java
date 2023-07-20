9309b73c4413b8d473bc42a14fbb0e3659119901
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-20 09:48:21 +0530

    Create isPrime.java

---
+++ Day9/isPrime.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode{
+     public int isPrime(int input1, int input2){
+
+
+
+        while(input1>1 && input1<5001){
+                    int n = input1/2;
+                    for(int i=2; i<=n; i++){
+                        if(input1%i == 0){
+                            return 1;
+        
+                        }}
+                    return 2;
+                    
+                    }
+                
+        		throw new UnsupportedOperationException("isPrime(int input1)");
+        	}
+        }
