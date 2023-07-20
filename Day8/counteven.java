849d10586190743aed6e520abad66e5131e27165
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-20 09:18:17 +0530

    Create counteven.java

---
+++ Day8/counteven.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode{
+     public int CountEvens(int input1, int input2, int input3, int input4, int input5){
+          int count;
+          int arr[] = new int[5];
+          arr[0] = input1;
+          arr[1] = input2;
+          arr[2] = input3;
+          arr[3] = input4;
+          arr[4] = input5;
+          for (int i = 0; i<5; i++){
+            if(a[i]%2==0 || a[i]==0){
+              count+=1;
+            }
+            return count;
+          }}}
+
+         
+
