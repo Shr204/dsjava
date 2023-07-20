542a75a2d0d07ffa82a1547232d74103e890158e
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-20 09:23:23 +0530

    Update counteven.java

--- Day8/counteven.java
+++ Day8/counteven.java
@@ -2,7 +2,7 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode{
      public int CountEvens(int input1, int input2, int input3, int input4, int input5){
-          int count;
+          int count = 0;
           int arr[] = new int[5];
           arr[0] = input1;
           arr[1] = input2;
@@ -10,7 +10,7 @@ class UserMainCode{
           arr[3] = input4;
           arr[4] = input5;
           for (int i = 0; i<5; i++){
-            if(a[i]%2==0 || a[i]==0){
+            if(arr[i]%2==0 || arr[i]==0){
               count+=1;
             }
             return count;
