2b3853851dd20b2a7325ea21cbbbcfee91164893
Shr204 <139195301+Shr204@users.noreply.github.com>
2023-07-20 09:24:06 +0530

    Create countodd.java

---
+++ Day8/countodd.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import  java.util.*;
+class UserMainCode{
+     public int CountEvens(int input1, int input2, int input3, int input4, int input5){
+          int count = 0;
+          int arr[] = new int[5];
+          arr[0] = input1;
+          arr[1] = input2;
+          arr[2] = input3;
+          arr[3] = input4;
+          arr[4] = input5;
+          for (int i = 0; i<5; i++){
+            if(arr[i]%2!=0 && arr[i]!=0){
+              count+=1;
+            }
+            return count;
+          }}}
